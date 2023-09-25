b3448dbc34342b492b9b30d000ce4daae0c92179
Ingrid S <110192683+ingridhermann@users.noreply.github.com>
2023-09-25 20:11:21 -0300

    Add files via upload

---
+++ queue-management.py
@@ -0,0 +1,10 @@
+queue = ['Ana', 'John', 'Marcus', 'Jake']
+
+# take an input
+name = input()
+
+# add the taken value to the end of the queue
+queue.append(name)
+
+# display the updated queue
+print(queue)
